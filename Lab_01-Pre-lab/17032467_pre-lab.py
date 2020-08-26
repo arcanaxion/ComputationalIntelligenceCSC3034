78c842a5e8c82e22ae37689bdd392063c23a00f7
arcanaxion <arcanaxion@gmail.com>
2020-08-26 11:01:46 +0800

    finished lab

--- Lab_01-Pre-lab/17032467_pre-lab.py
+++ Lab_01-Pre-lab/17032467_pre-lab.py
@@ -1,10 +1,51 @@
-def fibonacci(sl):
+import random
+from itertools import accumulate
+
+def fibonacci1(sl):
     if sl == 1:
         return 0
     elif sl == 2:
         return 1
     else:
-        return fibonacci(sl-2) + fibonacci(sl-1)
+        return fibonacci1(sl-2) + fibonacci1(sl-1)
+
+def fibonacci2(sl):
+    fib_seq = []
+    for i in range(sl):
+        if i == 0:
+            fib_seq.append(0)
+        elif i == 1:
+            fib_seq.append(1)
+        else:
+            fib_seq.append(fib_seq[-2] + fib_seq[-1])
+    return fib_seq
+
+def tossCoin():
+    head_chance = 0.2
+    if random.random() < head_chance:
+        headOrTail = "head"
+    else:
+        headOrTail = "tail"
+    return headOrTail 
+
+def chooseFromThree():
+    option_A = 0.2
+    option_B = 0.5
+    option_C = 0.3
+    options = list(accumulate([option_A, option_B, option_C]))
+    chance = random.random()
+    if chance < options[0]:
+        selectedOption = "A"
+    elif chance < options[1]:
+        selectedOption = "B"
+    else:
+        selectedOption = "C"
+    return selectedOption
+
 
 if __name__ == "__main__":
-    print(fibonacci(10)) 
+    print(fibonacci2(10)) 
+    
+    print(tossCoin())
+
+    print(chooseFromThree())
